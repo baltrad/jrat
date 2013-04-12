296c77a51d2f91877d8cfa9a69985d2fc0490f5a
Łukasz Wojtas <lukasz.wojtas@imgw.pl>
2013-04-12 13:34:36 +0200

    printing array to txt file

---
+++ src/pl/imgw/jrat/data/parsers/VolumeParser.java
@@ -0,0 +1,20 @@
+/**
+ * (C) 2013 INSTITUT OF METEOROLOGY AND WATER MANAGEMENT
+ */
+package pl.imgw.jrat.data.parsers;
+
+import pl.imgw.jrat.data.containers.VolumeContainer;
+
+/**
+ *
+ *  /Class description/
+ *
+ *
+ * @author <a href="mailto:lukasz.wojtas@imgw.pl">Lukasz Wojtas</a>
+ * 
+ */
+public interface VolumeParser {
+
+    public VolumeContainer getVolume();
+    
+}
---
+++ src/pl/imgw/jrat/tools/out/ArrayFilePrinter.java
@@ -0,0 +1,58 @@
+/**
+ * (C) 2013 INSTITUT OF METEOROLOGY AND WATER MANAGEMENT
+ */
+package pl.imgw.jrat.tools.out;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import pl.imgw.jrat.data.arrays.ArrayData;
+
+/**
+ *
+ *  /Class description/
+ *
+ *
+ * @author <a href="mailto:lukasz.wojtas@imgw.pl">Lukasz Wojtas</a>
+ * 
+ */
+public class ArrayFilePrinter {
+
+    public static void printTXT(ArrayData array, File outputFile) {
+
+        PrintWriter pw = null;
+        
+        int xSize = array.getSizeX();
+        int ySize = array.getSizeY();
+
+        try {
+            if (!outputFile.exists())
+                outputFile.createNewFile();
+            pw = new PrintWriter(new FileOutputStream(outputFile, false), true);
+            
+            for (int y = 0; y < ySize; y++) {
+                for (int x = 0; x < xSize; x++) {
+                    pw.print(array.getPoint(x, y) + " ");
+                }
+                pw.print("\n");
+            }
+
+            LogHandler.getLogs()
+                    .displayMsg("Array data save to file: " + outputFile,
+                            LogHandler.NORMAL);
+            
+        } catch (FileNotFoundException e) {
+
+        } catch (IOException e) {
+
+        } finally {
+            if (pw != null)
+                pw.close();
+        }
+
+    }
+    
+}
